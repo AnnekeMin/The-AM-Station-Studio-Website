f5c6b5a445a8071e609c50e5ccb114d76b02172b
AnnekeMin <53275342+AnnekeMin@users.noreply.github.com>
2025-08-20 17:18:46 -0400

    Create designs.bash

---
+++ designs.bash
@@ -0,0 +1,7 @@
+images/designs/
+  coastal-calm-living-room.jpg
+  shadow-starlight-bedroom.jpg
+  isla-inspired-kitchen.jpg
+  reader-nook-oak-sage.jpg
+  entryway-moss-stone.jpg
+  studio-black-gold.jpg
